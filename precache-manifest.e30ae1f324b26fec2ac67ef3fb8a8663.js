d214977fdb880c77207956182410913f398da5eb
Janet Wang <janet.wang.13@gmail.com>
2020-10-12 21:21:12 -0400

    Updates

---
+++ precache-manifest.e30ae1f324b26fec2ac67ef3fb8a8663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d2888900a22707a3ffa08ba92e8e6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fccc0cf65c5e0e9b648",
+    "url": "/static/css/main.8d0a2227.chunk.css"
+  },
+  {
+    "revision": "6f1d5d87bcfd2d6ff21a",
+    "url": "/static/js/2.e0135bc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e0135bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fccc0cf65c5e0e9b648",
+    "url": "/static/js/main.0a777429.chunk.js"
+  },
+  {
+    "revision": "2ae4eee30e5377da88dc",
+    "url": "/static/js/runtime-main.d3f61c46.js"
+  }
+]);
